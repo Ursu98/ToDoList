53dee58a7002fdac1376818e95f6963bc1197959
AvaG <avagheana@gmail.com>
2022-10-27 22:42:17 +0300

    script-throughtext

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
-const checkBox = document.querySelector('.checkbox-through')
+const textThrough = document.querySelector('.text-through');
+let count = 0;
+textThrough.addEventListener('click', () => {
+    textThrough.style.textDecoration = 'line-through';
+    count++;
+    if (count % 2 === 0) {
+        textThrough.style.textDecoration = 'none';
+    }
+});
 
-checkBox.addEventListener('click', (ev) =>{
-   git
-})
+
