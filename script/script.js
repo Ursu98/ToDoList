888c3376eb2fdd9bd2d206ba607aae2d03c158b8
Ursu98 <ursuandrei5985@gmail.com>
2022-10-28 09:09:54 +0300

    add remove

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-
 let textThrough = document.querySelectorAll('.text-through');
 const task_list = document.querySelector('.add-lists')
 const button_add_task = document.querySelector('.add-btn')
@@ -6,7 +5,7 @@ const button_add_task = document.querySelector('.add-btn')
 
 let count = 0;
 
-textThrough.forEach(item =>{
+textThrough.forEach(item => {
 
     item.addEventListener('click', () => {
         item.style.textDecoration = 'line-through';
@@ -14,19 +13,18 @@ textThrough.forEach(item =>{
         if (count % 2 === 0) {
             item.style.textDecoration = 'none';
         }
-})
+    })
 });
 
 
-
 //Event to add tasks
-button_add_task.addEventListener('click', ()=>{
+button_add_task.addEventListener('click', () => {
     let text = document.querySelector('.text-list').value;
     const logo = document.createElement('i')
-    logo.classList.add('fa','fa-trash','fa-2x')
-    if(text === ''){
+    logo.classList.add('fa', 'fa-trash', 'fa-2x', 'close')
+    if (text === '') {
         alert("Write some text!")
-    }else{
+    } else {
         const li = document.createElement("li");
         li.classList.add('text-through')
         li.append(text)
@@ -35,6 +33,13 @@ button_add_task.addEventListener('click', ()=>{
 
     }
     document.querySelector('.text-list').value = '';
-    })
+})
 
-
+const close = document.getElementsByClassName("close");
+let i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        const li = this.parentElement;
+        li.style.display = "none";
+    }
+}
