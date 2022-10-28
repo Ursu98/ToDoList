7358f29dd139cef75ecb18ccbfa48c755c3f5e7c
FoxArt <torixcity@gmail.com>
2022-10-28 19:51:56 +0300

    reworked html css js

--- script/script.js
+++ script/script.js
@@ -1,45 +1,43 @@
-let textThrough = document.querySelectorAll('.text-through');
-const task_list = document.querySelector('.add-lists')
-const button_add_task = document.querySelector('.add-btn')
+const textThrough = document.querySelectorAll(".task-container");
+const task_list = document.querySelector(".add-lists");
+const button_add_task = document.querySelector(".add-btn");
+const remove_btn = document.querySelectorAll(".remove");
+const label = document.querySelectorAll("label");
 
-
-let count = 0;
-
-textThrough.forEach(item => {
-
-    item.addEventListener('click', () => {
-        item.style.textDecoration = 'line-through';
-        count++;
-        if (count % 2 === 0) {
-            item.style.textDecoration = 'none';
-        }
-    })
+const textThrough1 = [];
+textThrough.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("text-through");
+  });
 });
 
+button_add_task.addEventListener("click", () => {
+  let text = document.querySelector(".text-list").value;
 
-//Event to add tasks
-button_add_task.addEventListener('click', () => {
-    let text = document.querySelector('.text-list').value;
-    const logo = document.createElement('i')
-    logo.classList.add('fa', 'fa-trash', 'fa-2x', 'close')
-    if (text === '') {
-        alert("Write some text!")
-    } else {
-        const li = document.createElement("li");
-        li.classList.add('text-through')
-        li.append(text)
-        li.append(logo)
-        task_list.appendChild(li);
-
+  if (text === "") {
+    alert("Write some text!");
+  } else {
+    const li = document.createElement("div");
+    console.log(li);
+    li.classList.toggle("task-container");
+    li.innerHTML = `<input type="checkbox"><li>${text}</li><button class="remove"><i class="fa fa-trash"></i></button>`;
+    task_list.append(li);
+    textThrough1.push(li);
+    console.log(textThrough1);
+    for (let i = 0; i < textThrough1.length; i++) {
+      textThrough1[i].onclick = function () {
+        const remove_li = this;
+        remove_li.style.display = "none";
+      };
     }
-    document.querySelector('.text-list').value = '';
-})
+  }
 
-const close = document.getElementsByClassName("close");
-let i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        const li = this.parentElement;
-        li.style.display = "none";
-    }
+  document.querySelector(".text-list").value = "";
+});
+
+for (let i = 0; i < remove_btn.length; i++) {
+  remove_btn[i].onclick = function () {
+    const div = this.parentNode;
+    div.style.display = "none";
+  };
 }
